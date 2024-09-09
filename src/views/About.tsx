6afa462ba26186a5a7bbed4cfa9b36b1d1ed1cd2
soswedan <101846470+soswedan@users.noreply.github.com>
2024-09-09 16:49:05 -0600

    added epic card tilt effect

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import VanillaTilt from "vanilla-tilt";
 import { execs } from "../data/Execs";
 
 function About() {
   const [loadedImages, setLoadedImages] = useState<number[]>([]);
   const [allImagesLoaded, setAllImagesLoaded] = useState(false);
+  const cardRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   useEffect(() => {
     const imagePromises = execs.map((exec, index) => {
@@ -22,31 +24,49 @@ function About() {
     });
   }, []);
 
+  useEffect(() => {
+    if (allImagesLoaded) {
+      cardRefs.current.forEach((card) => {
+        if (card) {
+          VanillaTilt.init(card, {
+            max: 25,
+            speed: 400,
+            glare: true,
+            "max-glare": 0.5,
+          });
+        }
+      });
+    }
+  }, [allImagesLoaded]);
+
   return (
-    <div>
-      <div className="container mx-auto p-4">
-        <h1 className="text-4xl font-bold text-center text-lavendar">
-          Executives
-        </h1>
-      </div>
-      {allImagesLoaded ? (
-        <div className="flex flex-wrap justify-center gap-4 p-4">
-          {execs.map((exec, index) => (
-            <div key={exec.id} className="max-w-sm m-4 shiny-card">
-              <img
-                className={`w-full h-auto object-cover rounded-lg border-2 border-lavendar fade-in ${loadedImages.includes(index) ? "loaded" : ""}`}
-                src={exec.card}
-                alt={`${exec.firstName} ${exec.lastName}`}
-              />
-            </div>
-          ))}
+      <div>
+        <div className="container mx-auto p-4">
+          <h1 className="text-4xl font-bold text-center text-lavendar">
+            Executives
+          </h1>
         </div>
-      ) : (
-        <div className="spinner"></div>
-      )}
-    </div>
+        {allImagesLoaded ? (
+            <div className="flex flex-wrap justify-center gap-4 p-4">
+              {execs.map((exec, index) => (
+                  <div
+                      key={exec.id}
+                      className="max-w-sm m-4 shiny-card"
+                      ref={(el) => (cardRefs.current[index] = el)}
+                  >
+                    <img
+                        className={`w-full h-auto object-cover rounded-lg border-2 border-lavendar fade-in ${loadedImages.includes(index) ? "loaded" : ""}`}
+                        src={exec.card}
+                        alt={`${exec.firstName} ${exec.lastName}`}
+                    />
+                  </div>
+              ))}
+            </div>
+        ) : (
+            <div className="spinner"></div>
+        )}
+      </div>
   );
 }
 
 export default About;
-
