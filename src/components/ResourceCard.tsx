d1ab57ef9fd8c341f0d96a9a00c46f984056e818
soswedan <101846470+soswedan@users.noreply.github.com>
2024-09-10 00:38:28 -0600

    added tilt effect to resource cards

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import VanillaTilt from 'vanilla-tilt';
 
 interface ResourceCardProps {
     category: string;
@@ -8,8 +9,27 @@ interface ResourceCardProps {
 }
 
 const ResourceCard: React.FC<ResourceCardProps> = ({ category, title, link, description }) => {
+    const cardRef = useRef<HTMLAnchorElement | null>(null);
+
+    useEffect(() => {
+        if (cardRef.current) {
+            VanillaTilt.init(cardRef.current, {
+                max: 10,
+                speed: 400,
+                glare: true,
+                "max-glare": 0.5,
+            });
+        }
+    }, []);
+
     return (
-        <a href={link} className="block bg-like-lavendar bg-opacity-30 rounded-lg p-4 m-2  h-60 w-60" target="_blank" rel="noopener noreferrer">
+        <a
+            href={link}
+            className="block bg-like-lavendar bg-opacity-30 rounded-lg p-4 m-2 h-60 w-60"
+            target="_blank"
+            rel="noopener noreferrer"
+            ref={cardRef}
+        >
             <h3 className="text-lg font-bold mb-2 text-lavendar">{title}</h3>
             <p className="text-sm text-like-lavendar mb-2">{category}</p>
             <p className="text-sm mb-4 text-lavendar">{description}</p>
