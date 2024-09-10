a52b016ad14ba89a6631987f206181f7b14adb53
Nicholas MacKinnon <nicholasmackinnon9@gmail.com>
2024-09-10 12:07:18 -0600

    404_thot_found

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,13 +1,16 @@
+import goob from "../assets/constructionGoob.png"
+
 function NotFound() {
     return (
-
-
-        <div className="text-center">
-            <h1>404 - Not Found</h1>
-            <p>The page you're looking for does not exist.</p>
+        <div className="flex justify-center h-full w-full overflow-hidden items-center">
+            <div className="text-center text-white">
+                <div className="my-5">
+                    <h1>404 - Not Found</h1>
+                    <p>The page you're looking for does not exist.</p>
+                </div>
+                <img src={goob} className="inline w-1/2 md:w-1/4 lg:w-1/6 xl:w-1/8" />
+            </div>
         </div>
-
-
     );
 }
 
