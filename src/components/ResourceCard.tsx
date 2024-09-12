2c4f09c76f04bf0361c694c7e6cd73b191c752ed
Dante <dkirsman@gmail.com>
2024-09-11 23:14:21 -0600

    feat(calendar): add calendar

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -1,41 +1,47 @@
-import React, { useRef, useEffect } from 'react';
-import VanillaTilt from 'vanilla-tilt';
+import React, { useRef, useEffect } from "react";
+import VanillaTilt from "vanilla-tilt";
 
 interface ResourceCardProps {
-    category: string;
-    title: string;
-    link: string;
-    description: string;
+  category: string;
+  title: string;
+  link: string;
+  description: string;
 }
 
-const ResourceCard: React.FC<ResourceCardProps> = ({ category, title, link, description }) => {
-    const cardRef = useRef<HTMLAnchorElement | null>(null);
+const ResourceCard: React.FC<ResourceCardProps> = ({
+  category,
+  title,
+  link,
+  description,
+}) => {
+  const cardRef = useRef<HTMLAnchorElement | null>(null);
 
-    useEffect(() => {
-        if (cardRef.current) {
-            VanillaTilt.init(cardRef.current, {
-                max: 10,
-                speed: 400,
-                glare: true,
-                "max-glare": 0.5,
-            });
-        }
-    }, []);
+  useEffect(() => {
+    if (cardRef.current) {
+      VanillaTilt.init(cardRef.current, {
+        max: 10,
+        speed: 400,
+        glare: true,
+        "max-glare": 0.5,
+      });
+    }
+  }, []);
 
-    return (
-        <a
-            href={link}
-            className="block bg-like-lavendar bg-opacity-30 rounded-lg p-4 m-2 h-60 w-60"
-            target="_blank"
-            rel="noopener noreferrer"
-            ref={cardRef}
-            data-aos="fade-up"
-        >
-            <h3 className="text-lg font-bold mb-2 text-lavendar">{title}</h3>
-            <p className="text-sm text-like-lavendar mb-2">{category}</p>
-            <p className="text-sm mb-4 text-lavendar">{description}</p>
-        </a>
-    );
+  return (
+    <a
+      href={link}
+      className="block bg-like-lavendar bg-opacity-30 rounded-lg p-4 m-2 h-60 w-60"
+      target="_blank"
+      rel="noopener noreferrer"
+      ref={cardRef}
+      data-aos="fade-up"
+    >
+      <h3 className="text-lg font-bold mb-2 text-lavendar">{title}</h3>
+      <p className="text-sm text-like-lavendar mb-2">{category}</p>
+      <p className="text-sm mb-4 text-lavendar">{description}</p>
+    </a>
+  );
 };
 
 export default ResourceCard;
+
--- src/views/Calendar.tsx
+++ src/views/Calendar.tsx
@@ -1,9 +1,99 @@
+import { useState } from "react";
+import { events } from "../data/events";
+import {
+  isSameDay,
+  format,
+  addMonths,
+  subMonths,
+  getMonth,
+  getYear,
+} from "date-fns";
 
-function Calendar(){
+import ForwardIcon from "../images/icons/forward.svg";
+import BackIcon from "../images/icons/back.svg";
+import CalendarCard from "../components/CalendarCard";
+
+function Calendar() {
+  const [currentDate, setCurrentDate] = useState<Date>(new Date());
+
+  const nextMonth = () => setCurrentDate((prevDate) => addMonths(prevDate, 1));
+  const prevMonth = () => setCurrentDate((prevDate) => subMonths(prevDate, 1));
+
+  const currentMonth = getMonth(currentDate);
+  const currentYear = getYear(currentDate);
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+  const startDay = new Date(currentYear, currentMonth, 1).getDay();
+
+  const getWeekdays = () => {
+    return ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  };
+
+  const filteredEvents = events
+    .filter((event) => {
+      const eventDate = new Date(event.date);
+      return (
+        getMonth(eventDate) === currentMonth &&
+        getYear(eventDate) === currentYear
+      );
+    })
+    .sort((a, b) => {
+      const [aHour, aMinute] = a.time.split(":").map(Number);
+      const [bHour, bMinute] = b.time.split(":").map(Number);
+      if (a.date === b.date) {
+        return aHour - bHour || aMinute - bMinute;
+      }
+      return new Date(a.date).getTime() - new Date(b.date).getTime();
+    });
+
+  const daysArray = Array.from({ length: daysInMonth }, (_, i) => {
+    const date = new Date(currentYear, currentMonth, i + 1);
+    return {
+      date,
+      dayNumber: i + 1,
+      events: filteredEvents.filter((event) =>
+        isSameDay(date, new Date(event.date + "T" + event.time)),
+      ),
+    };
+  });
+
   return (
+    <div className="container mx-auto p-4">
+      <div className="flex justify-center items-center mb-6">
+        <button onClick={prevMonth}>
+          <img src={BackIcon} className="h-10" />
+        </button>
+        <h2 className="text-4xl font-bold text-center text-lavendar mx-4 ">
+          {format(currentDate, "MMM yyyy")}
+        </h2>
+        <button onClick={nextMonth}>
+          <img src={ForwardIcon} className="h-10" />
+        </button>
+      </div>
 
-      <div>Under Construction</div>
+      <div className="grid grid-cols-7 text-center font-bold mb-2 text-lavendar">
+        {getWeekdays().map((day, index) => (
+          <div key={index} className="p-2 text-xl">
+            {day}
+          </div>
+        ))}
+      </div>
 
+      <div className="grid grid-cols-7 gap-1">
+        {Array.from({ length: startDay }).map((_, index) => (
+          <div
+            key={index}
+            className="rounded-lg bg-like-lavendar bg-opacity-30 h-32 diagonal-lines"
+          ></div>
+        ))}
+        {daysArray.map((day, index) => (
+          <CalendarCard
+            key={index}
+            day={day}
+            isToday={day.date.toDateString() === new Date().toDateString()}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
