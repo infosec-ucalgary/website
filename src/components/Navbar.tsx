bbaed14b8a3e8f3c46fa2a16236a58b4afb4a527
Nicholasm24 <nicholasmackinnon9@gmail.com>
2024-09-12 23:33:23 -0600

    fixed the navbar bug
    added a second div that only appears when the burger is spawned so that the navbar menu pops up below the logo and burger.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,46 +19,63 @@ function Navbar() {
   ];
 
   return (
-    <nav className="flex items-center justify-between bg-gradient-to-r from-like-lavendar to-electric-plum p-2">
-      <div className="flex items-center flex-shrink-0 text-lavendar mr-6 ml-3">
-        <Link to="/" className="flex items-center">
-          <img className="h-12 w-12 mr-2" src={Logo} />
-          <img className="h-8 hidden navbarCustom:block" src={CYBERSEC} />
-        </Link>
-      </div>
-      <div className="block lg:hidden">
-        <button
-          className="flex items-center px-3 py-2 border rounded text-gray-300 border-gray-500 hover:text-white hover:neutral-gray"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+    <nav className="bg-gradient-to-r from-like-lavendar to-electric-plum p-2">
+      <div className="flex items-center justify-between ">
+        <div className="flex items-center flex-shrink-0 text-lavendar mr-6 ml-3">
+          <Link to="/" className="flex items-center">
+            <img className="h-12 w-12 mr-2" src={Logo} />
+            <img className="h-8 hidden navbarCustom:block" src={CYBERSEC} />
+          </Link>
+        </div>
+        <div className="block lg:hidden">
+          <button
+            className="flex items-center px-3 py-2 border rounded text-gray-300 border-gray-500 hover:text-white hover:neutral-gray"
+            onClick={() => setIsOpen(!isOpen)}
           >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div className={`w-full flex-grow lg:flex lg:items-center lg:w-auto hidden`}>
+          <div className="flex-grow flex justify-end">
+            {navItems.map((item) => (
+              <Link
+                key={item.name}
+                className={`block mt-4 lg:inline-block lg:mt-0 text-white hover:text-black text-lg mr-8 transition duration-300 ${
+                  location.pathname === item.to ? "underline" : ""
+                }`}
+                to={item.to}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
       <div
-        className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${
-          isOpen ? "block" : "hidden"
-        }`}
-      >
-        <div className="text-sm lg:flex-grow lg:flex lg:justify-end">
-          {navItems.map((item) => (
-            <Link
-              key={item.name}
-              className={`block mt-4 lg:inline-block lg:mt-0 text-white hover:text-black text-lg mr-8 transition duration-300 ${
-                location.pathname === item.to ? "underline" : ""
-              }`}
-              to={item.to}
-            >
-              {item.name}
-            </Link>
-          ))}
-        </div>
+          className={`w-full lg:hidden flex-grow lg:items-center lg:w-auto ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
+          <div className="text-sm flex-col">
+            {navItems.map((item) => (
+              <Link
+                key={item.name}
+                className={`flex my-2 justify-center items-center lg:mt-0 text-white hover:text-black text-lg transition duration-300 ${
+                  location.pathname === item.to ? "underline" : ""
+                }`}
+                to={item.to}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
       </div>
     </nav>
   );
