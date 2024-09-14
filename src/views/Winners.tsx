ead494b06e44c54766c9600d9d4fb006d9746038
soswedan <101846470+soswedan@users.noreply.github.com>
2024-09-13 22:03:49 -0600

    added a title to the "CTF Winners" page

--- src/views/Winners.tsx
+++ src/views/Winners.tsx
@@ -1,7 +1,9 @@
 function Winners() {
     return (
 
-        <div>Under Construction</div>
+        <div><h1 className="text-4xl font-bold text-center text-lavendar py-20">
+            CTF Winners will be announced soon!
+        </h1></div>
 
     )
 }
