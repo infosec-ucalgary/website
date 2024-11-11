17fa4e13613dfc7bca0b12f1f2bb4300ada46064
Dante <dkirsman@gmail.com>
2024-11-11 10:57:39 -0700

    feat(courses page): show courses page

---
+++ src/views/Courses.tsx
@@ -0,0 +1,9 @@
+function Courses() {
+  return (
+    <div className="h-[75vh] flex items-center justify-center text-white text-8xl">
+      TO BE ANNOUNCED
+    </div>
+  );
+}
+
+export default Courses;
