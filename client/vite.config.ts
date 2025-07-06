313e23b7295881a202c2644731ff9cfa87d6e1cb
Dante <dkirsman@gmail.com>
2025-07-06 10:49:20 -0600

    feat: ✨ setup tanstack router

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       "@client": path.resolve(__dirname, "./src"),
