77ea5c2b62e9efa6f7d87fa25f5f3f741e6b6460
Dante <dkirsman@gmail.com>
2025-07-06 10:10:31 -0600

    refactor: 🔨 delete everything to switch to tanstack

--- src/data/courses.ts
+++
@@ -1,7 +0,0 @@
-export const ccsa_images = [
-  "/images/courses/ccsa/1.webp",
-  "/images/courses/ccsa/2.webp",
-  "/images/courses/ccsa/3.webp",
-  "/images/courses/ccsa/4.webp",
-  "/images/courses/ccsa/5.webp",
-];
