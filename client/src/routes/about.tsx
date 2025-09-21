6e48cd7b9d7c4564cb8f6050254742f6b5b5981b
Dante <github.com.cabdriver824@passmail.com>
2025-09-20 23:43:28 -0600

    fix(layout): use flex to center cards

--- client/src/routes/about.tsx
+++ client/src/routes/about.tsx
@@ -8,17 +8,18 @@ export const Route = createFileRoute('/about')({
 
 function About() {
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 min-[96rem]:grid-cols-4 gap-4 p-8">
+    <div className="flex flex-wrap justify-center p-8">
       {PROFILE_CARDS.map((card, index) => (
-        <ProfileCard
-          key={index}
-          name={card.name}
-          role={card.role}
-          roleDetails={card.roleDetails}
-          fact={card.fact}
-          src={card.headshot}
-          hobbies={card.hobbies}
-        />
+        <div className="w-1/4 p-2" key={index}>
+          <ProfileCard
+            name={card.name}
+            role={card.role}
+            roleDetails={card.roleDetails}
+            fact={card.fact}
+            src={card.headshot}
+            hobbies={card.hobbies}
+          />
+        </div>
       ))}
     </div>
   )
