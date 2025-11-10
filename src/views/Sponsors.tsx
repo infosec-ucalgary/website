70c04c3e6fd45b015e989e3cb2b0c4ef196d5740
GabrielSew <gabrielsewaldmoises@gmail.com>
2025-11-09 23:56:08 -0700

    added fortinet to sponsors

--- src/views/Sponsors.tsx
+++ src/views/Sponsors.tsx
@@ -2,7 +2,9 @@ import "aos/dist/aos.css";
 import Slideshow from "../components/Slideshow";
 import { enfocom_images } from "../data/sponsors";
 import ecCouncil from "../../public/images/sponsors/ecCouncil/EcCouncil_01.jpg"
-import ecCouncilBanner from "../../public/images/sponsors/ecCouncil/ecCouncil_banner.png" 
+import ecCouncilBanner from "../../public/images/sponsors/ecCouncil/ecCouncil_banner.png"
+import fortinetLogo from "../../public/images/sponsors/fortinet/fortinet-logo-2.png" 
+import fortinetBanner from "../../public/images/sponsors/fortinet/fortinet-banner.jpg"
 
 function Sponsors() {
 
@@ -16,7 +18,15 @@ function Sponsors() {
                 <a href="https://www.enfocomcyber.com/" className="m-12">
                     <img src="/images/sponsors/enfocom/enfocom-cyber-logo_white-text.webp" className="w-[40rem] max-w-[95vw] hover:brightness-50 transition-all ease-linear duration-150"/>
                 </a>
+
                 
+                <div className=" mt-8">
+                    <img src={fortinetBanner} className="w-[40rem] h-[45rem]"/>
+                </div>
+                <a href="https://training.fortinet.com/" className="m-12">
+                    <img src={fortinetLogo} className="w-[40rem] max-w-[95vw] hover:brightness-50 transition-all ease-linear duration-150"/>
+                </a>
+
                 <div className=" mt-8">
                     <img src={ecCouncil} className="w-[40rem] h-[40rem]"/>
                 </div>
