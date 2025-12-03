473a98543f75930d151e4372ee2b5323a6b025a7
Mcveigth <102336370+mcveigth@users.noreply.github.com>
2025-12-03 16:22:07 -0700

    Add update script for CyberSec UCalgary website
    This script updates the CyberSec UCalgary website by pulling the latest changes, updating dependencies, building the app, and deploying it to the web server.

---
+++ update-cybersec-app.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+NODE_OPTIONS=--openssl-legacy-provider
+echo Updating CyberSec UCalgary website...
+
+# Pull changes into app directory
+cd /home/admin/repos/website
+git fetch --all
+git reset --hard origin/main
+git pull
+
+# Make sure NPM is updated and stuff
+rm -rf node_modules
+npm cache clean --force
+npm ci
+
+# Build app
+npm run build
+
+# Delete app dist folder from /usr/share/nginx/html/dist
+rm -rf /usr/share/nginx/html/dist
+
+# Copy freshly built app into the just deleted directory
+cp -r dist /usr/share/nginx/html/cybersec
+
+echo Done.
