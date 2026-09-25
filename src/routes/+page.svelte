8531a318d6864527f352feec7e6dbc970a1ec9da
Hadi Faraz <hadifaraz52@protonmail.com>
2026-09-25 23:08:54 +0000

    Add homegrid to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,25 @@
 <!-- This is the landing page -->
 
+<script lang="ts">
+	import HomeGrid from "$lib/components/HomeGrid.svelte";
+	import HomeGridBox from "$lib/components/HomeGridBox.svelte";
+</script>
+
 <h1 class="sr-only">UCalgary Cyber Security Club</h1>
 
 <div class="home">
-	
+	<HomeGrid>
+		<HomeGridBox size="hero">
+			<h2>Upcoming Event</h2>
+		</HomeGridBox>
+		<HomeGridBox size="small">
+			<h2>Mailing List</h2>
+			<a>Join now!</a>
+		</HomeGridBox>
+		<HomeGridBox size="tall">
+			<h2>Latest Post</h2>
+		</HomeGridBox>
+	</HomeGrid>
 </div>
 
 <style>
@@ -22,7 +38,7 @@
 	.home {
 		max-width: var(--content-max-width);
 		margin: 0 auto;
-		padding: 2rem 1.25rem;
+		padding: 2rem var(--page-gutter);
 		display: flex;
 		flex-direction: column;
 		gap: 2rem;
