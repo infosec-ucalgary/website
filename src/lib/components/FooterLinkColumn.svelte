5291c35deb0b8356be23e3c3855601070ae9f009
Hadi Faraz <hadifaraz52@protonmail.com>
2026-09-25 21:07:29 +0000

    Start theming footer link column

---
+++ src/lib/components/FooterLinkColumn.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import { isExternal } from '$lib/links';
+
+	type Link = {
+		display: string;
+		href: string;
+	};
+
+	type Props = {
+		title: string;
+		links: Link[];
+	};
+
+	let { title, links }: Props = $props();
+</script>
+
+<div class="link-column-container">
+	<div class="link-column-inner">
+		<h6 class="column-title">{title}</h6>
+		<!-- eslint-disable svelte/no-navigation-without-resolve -->
+        <hr>
+		<ul class="column-links">
+			{#each links as link (link.display)}
+				<li>
+					<a
+						class="column-link"
+						href={link.href}
+						target={isExternal(link.href) ? '_blank' : undefined}
+						rel={isExternal(link.href) ? 'noreferrer noopener' : undefined}
+					>
+						{link.display}
+					</a>
+				</li>
+			{/each}
+		</ul>
+		<!-- eslint-enable svelte/no-navigation-without-resolve -->
+	</div>
+</div>
+
+<style>
+    .link-column-container {
+        max-width: fit-content;
+    }
+
+    .column-title {
+        padding-bottom: 0;
+        margin-bottom: 0;
+    }
+
+	.column-links {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+    .column-link {
+        color: var(--color-muted);
+        font-size: 12px;
+    }
+
+    .column-link:hover {
+        text-decoration: underline;
+    }
+
+    hr {
+        margin-top: 0;
+        padding-top: 0;
+    }
+</style>
